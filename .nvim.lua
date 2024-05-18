cfa49bf0c149d003f9ebae71643ed51eade250fc
EnergoStalin <ens.stalin@gmail.com>
2024-05-18 13:04:15 +0300

    chore(nvim): remove overlapping shortcut

--- .nvim.lua
+++ .nvim.lua
@@ -4,22 +4,8 @@ overseer.register_template({
   name = 'build ppxiv',
   builder = function()
     return {
-      cmd = 'python build.py',
+      cmd = 'python build.py && firefox output/ppixiv-main.user.js',
       cwd = vim.fn.getcwd(),
     }
   end,
 })
-
-vim.keymap.set('n', '<c-i>', function()
-  overseer.new_task({
-    name = 'Build and install',
-    components = { 'unique' },
-    strategy = {
-      'orchestrator',
-      tasks = {
-        'build ppxiv',
-        { 'shell', cmd = 'firefox output/ppixiv-main.user.js' },
-      },
-    }
-  }):start()
-end)
