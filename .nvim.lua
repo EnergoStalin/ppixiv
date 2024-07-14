f2b90efc22c8b6fe0409fc49d0cdbfef6134c110
EnergoStalin <ens.stalin@gmail.com>
2024-07-14 16:15:27 +0300

    build(nvim): refactor tasks

--- .nvim.lua
+++ .nvim.lua
@@ -1,10 +1,10 @@
 local overseer = require('overseer')
 
 overseer.register_template({
-  name = 'build ppixiv',
+  name = 'build',
   builder = function()
     return {
-      name = 'build ppixiv',
+      name = 'build',
       cmd = 'python ./build.py',
       cwd = vim.fn.getcwd(),
       components = {
@@ -16,10 +16,10 @@ overseer.register_template({
 })
 
 overseer.register_template({
-  name = 'install ppixiv',
+  name = 'install',
   builder = function()
     return {
-      name = 'install ppixiv',
+      name = 'install',
       cmd = 'firefox ./output/ppixiv-main.user.js',
       cwd = vim.fn.getcwd(),
       components = {
@@ -28,7 +28,7 @@ overseer.register_template({
         {
           'dependencies',
           task_names = {
-            'build ppixiv'
+            'build'
           }
         },
       }
@@ -39,20 +39,34 @@ overseer.register_template({
 overseer.run_template({ name = 'http', params = { cwd = vim.fn.getcwd(), path = './output/ppixiv-main.user.js', address = '0.0.0.0', port = 8080 } })
 
 overseer.register_template({
-  name = 'ppixiv AdbYandexBrowserOpen',
-  builder = function ()
+  name = 'android install dev',
+  builder = function()
     return {
-      name = 'ppixiv adb open',
-      cmd = '',
-      components = {
-        'default',
-        {
-          'dependencies',
-          task_names = {
-            { 'AdbYandexBrowserOpen', url = 'http://%local%:8080/ppixiv-main.user.js' }
+      name = 'adb open',
+      strategy = {
+        "orchestrator",
+        tasks = {
+          { 'AdbYandexBrowserOpen', url = 'http://%local%:8080/ppixiv-main.user.js' }
+        },
+      },
+    }
+  end
+})
+
+overseer.register_template({
+  name = 'android install git',
+  builder = function()
+    return {
+      name = 'adb open',
+      strategy = {
+        "orchestrator",
+        tasks = {
+          {
+            'AdbYandexBrowserOpen',
+            url = 'https://github.com/EnergoStalin/ppixiv/releases/latest/download/ppixiv-main.user.js'
           }
         },
-      }
+      },
     }
   end
 })
